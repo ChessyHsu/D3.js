9138279c8e36b2ad7b7bbefeb9949b7c97b7533d
ChessyHsu <chessy2206@gmail.com>
2016-11-16 18:24:36 +0800

    function

---
+++ funt.js
@@ -0,0 +1,20 @@
+function f(){
+	var width = 1024;
+	var height = 768;
+	var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
+	d3.csv("./weather.csv", 
+		function(data)
+		{
+			var ln = data.length;
+			console.log(data);
+			var maxy1 = d3.max(data, function(d){ return d.TX02; });
+			var lines1 = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.TX02*((	height)/maxy1)});	
+			ctrl.append("path	").data([data]).attr("d", lines1).attr("stroke", "red").attr("fill", "none");	
+	
+			var maxy2 = d3.max(data, function(d){ return d.TX04; });
+			var lines2 = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.TX04*((	height)/maxy1)});	
+			ctrl.append("path	").data([data]).attr("d", lines2).attr("stroke", "blue").attr("fill", "none");	
+			
+		}		
+	);
+}
